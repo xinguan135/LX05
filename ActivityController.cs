7aeaeabb0c253e12011fe4d1c68dab994c8ec125
xinguan135 <71052892+xinguan135@users.noreply.github.com>
2020-10-21 18:34:09 +0800

    Add files via upload

---
+++ ActivityController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public ActivityController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json(DAL.Activity.Instance.GetCount());
+        }
+        [HttpGet("verifyCount")]
+        public ActionResult GetVerifyCount()
+        {
+            return Json(DAL.Activity.Instance.GetVerifyCount());
+        }
+        [HttpGet("recommend")]
+        public ActionResult GetRecommend()  //获取推荐活动
+        {
+            var result = DAL.Activity.Instance.GetRecommend();
+            if (result != null)
+                return Json(Result.Ok(result));
+            else
+                return Json(Result.Err("记录数为0"));
+        }
+    }
+}
